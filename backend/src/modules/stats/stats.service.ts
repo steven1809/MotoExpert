03df6fff25d15254060b10f8f788ccb0f1d615c4
brandon0925sanchez-hub <brandon0925sanchez@gamil.com>
2026-06-02 10:12:25 -0500

    configuracion y cambios

--- backend/src/modules/stats/stats.service.ts
+++ backend/src/modules/stats/stats.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, Between } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Cita } from '../citas/entities/cita.entity';
 
 @Injectable()
